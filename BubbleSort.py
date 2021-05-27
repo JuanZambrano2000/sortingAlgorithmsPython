b98cfadf4fd49062dd7cc8661e67b4f42d518069
Juan Zambrano <jpablozambrano@hotmail.com>
2021-05-27 11:48:04 -0500

    v1.1
    Fixed bubbleSort, I delete data structures part

--- BubbleSort.py
+++ BubbleSort.py
@@ -4,8 +4,9 @@ class Solution:
         for i in range(n):
             for j in range(0, n-1-i):
                 if arr[j] > arr[j+1]:
-                    arr[j], arr[j+1] =  arr[j+1], arr[j]
-                    
+                    aux = arr[j]
+                    arr[j] = arr[j+1]
+                    arr[j+1] = aux                   
         # code here
 
 #{ 
